0e21f13de34586a09c4146e354a0212e41f59df1
León Sepúlveda <NECDE@users.noreply.github.com>
2017-11-15 00:27:03 -0300

    Add files via upload

--- spacex.py
+++ spacex.py
@@ -3,7 +3,7 @@ import sys
 
 def infospacex():
 	response = requests.get("https://api.spacexdata.com/v1/info")
-	payload = response.json() # list
+	payload = response.json()
 
 	print('\n-----------------------------------------\n')
 	print("\nSpaceX Info\n")
@@ -22,46 +22,44 @@ def infospacex():
 	print("Summary: ",payload['summary'])
 
 def lastflight():
-	
-	response = requests.get("https://api.spacexdata.com/v1/launches/latest")
-	payload = response.json() # list
 
-	if payload:
-		for i in payload:
-			print('\n-----------------------------------------\n')
-			print("\nLast Flight SpaceX\n")
-			print("Flight Number: ",i['flight_number'])
-			print("Launch Year: ",i['launch_year'])
-			print("Launch Date Unix: ",i['launch_date_unix'])
-			print("Launch Date (UTC)",i['launch_date_utc'])
-			print("Launch Date (LOCAL)",i['launch_date_local'])
-			print("Rocket ID: ",i['rocket']['rocket_id'])
-			print("Rocket Name: ",i['rocket']['rocket_name'])
-			print("Rocket Type: ",i['rocket']['rocket_type'])
-			print("Payload: ",i['payloads'][0]['payload_id']," of ",i['payloads'][0]['customers'])
-			print("Payload Type: ",i['payloads'][0]['payload_type'])
-			print("Orbit: ",i['payloads'][0]['orbit'])
-			print("\nDetails: ",i['details'])
-			print("\nLaunch ID: ",i['launch_site']['site_id'])
-			print("Launch Site: ",i['launch_site']['site_name'])
-			print("Launch Success:",i['launch_success'])
-			print("Land success:",i['land_success'])
-			print("Landing Type: ",i['landing_type'])
-			print("landing_vehicle", i['landing_vehicle'])
-			print("\nMission Patch: \n",i['links']['mission_patch'])
-			print("\nReddit Campaign: \n",i['links']['reddit_campaign'])
-			print("\nReddit Launch: \n",i['links']['reddit_launch'])
-			print("\nReddit Recovery: \n",i['links']['reddit_recovery'])
-			print("\nReddit Media: \n",i['links']['reddit_media'])
-			print("\nPresskit: \n",i['links']['presskit'])
-			print("\nArticle Link: \n",i['links']['article_link'])
-			print("\nVideo Link: \n",i['links']['video_link'])
-			print('\n-----------------------------------------\n')
+	response = requests.get("https://api.spacexdata.com/v1/launches/latest")
+	payload = response.json()
+
+	print('\n-----------------------------------------\n')
+	print("\nLast Flight SpaceX\n")
+	print("Flight Number: ",payload['flight_number'])
+	print("Launch Year: ",payload['launch_year'])
+	print("Launch Date Unix: ",payload['launch_date_unix'])
+	print("Launch Date (UTC)",payload['launch_date_utc'])
+	print("Launch Date (LOCAL)",payload['launch_date_local'])
+	print("Rocket ID: ",payload['rocket']['rocket_id'])
+	print("Rocket Name: ",payload['rocket']['rocket_name'])
+	print("Rocket Type: ",payload['rocket']['rocket_type'])
+	print("Payload: ",payload['payloads'][0]['payload_id']," of ",payload['payloads'][0]['customers'])
+	print("Payload Type: ",payload['payloads'][0]['payload_type'])
+	print("Orbit: ",payload['payloads'][0]['orbit'])
+	print("\nDetails: ",payload['details'])
+	print("\nLaunch ID: ",payload['launch_site']['site_id'])
+	print("Launch Site: ",payload['launch_site']['site_name'])
+	print("Launch Success:",payload['launch_success'])
+	print("Land success:",payload['land_success'])
+	print("Landing Type: ",payload['landing_type'])
+	print("landing_vehicle", payload['landing_vehicle'])
+	print("\nMission Patch: \n",payload['links']['mission_patch'])
+	print("\nReddit Campaign: \n",payload['links']['reddit_campaign'])
+	print("\nReddit Launch: \n",payload['links']['reddit_launch'])
+	print("\nReddit Recovery: \n",payload['links']['reddit_recovery'])
+	print("\nReddit Media: \n",payload['links']['reddit_media'])
+	print("\nPresskit: \n",payload['links']['presskit'])
+	print("\nArticle Link: \n",payload['links']['article_link'])
+	print("\nVideo Link: \n",payload['links']['video_link'])
+	print('\n-----------------------------------------\n')
 
 def upcomingflight():
 
 	response = requests.get("https://api.spacexdata.com/v1/launches/upcoming")
-	payload = response.json() # list
+	payload = response.json()
 
 	if payload:
 		for i in payload:
